5c8357ad4f2efa330c715b60120bfeea26965c6a
Michal Bojanowski <bojanowski.michal89@gmail.com>
2018-04-24 19:29:51 +0200

    User AnswerMapper implicitly in QuestionMapper

--- src/main/java/com/bocian/quizzes/api/v1/mapper/QuestionMapper.java
+++ src/main/java/com/bocian/quizzes/api/v1/mapper/QuestionMapper.java
@@ -1,15 +1,13 @@
 package com.bocian.quizzes.api.v1.mapper;
 
 import com.bocian.quizzes.api.v1.model.QuestionDTO;
+import com.bocian.quizzes.model.Question;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
-import com.bocian.quizzes.model.Question;
 
-import java.util.stream.Collectors;
-
-@Mapper
+@Mapper(uses = AnswerMapper.class)
 public interface QuestionMapper {
     QuestionMapper INSTANCE = Mappers.getMapper(QuestionMapper.class);
 
@@ -18,20 +16,8 @@ public interface QuestionMapper {
     })
     Question questionDTOToQuestion(QuestionDTO questionDTO);
 
-    default QuestionDTO questionToQuestionDTO(Question question) {
-        if ( question == null ) {
-            return null;
-        }
-
-        QuestionDTO questionDTO = new QuestionDTO();
-
-        questionDTO.setDurationInMinutes( question.getDurationMinutes() );
-        questionDTO.setType( question.getType() );
-        questionDTO.setDifficulty( question.getDifficulty() );
-        questionDTO.setDescription( question.getDescription() );
-        questionDTO.setTag( question.getTag() );
-        questionDTO.setAnswers(question.getAnswers().stream().map(AnswerMapper.INSTANCE::answerToAnswerDTO).collect(Collectors.toSet()));
-
-        return questionDTO;
-    }
+    @Mappings({
+            @Mapping(target = "durationInMinutes", source = "durationMinutes")
+    })
+    QuestionDTO questionToQuestionDTO(Question question);
 }
