3e7da97e7b696223e4fbca029df168e6bffcdff5
Michal Bojanowski <bojanowski.michal89@gmail.com>
2018-05-01 13:35:57 +0200

    quick answerControllerTest fix

--- src/test/java/com/bocian/quizzes/controllers/v1/AnswerControllerTest.java
+++ src/test/java/com/bocian/quizzes/controllers/v1/AnswerControllerTest.java
@@ -47,12 +47,12 @@ public class AnswerControllerTest {
         final String expectedResponseValue = "{\"answers\":[{\"id\":1,\"description\":\"desc\",\"is_correct\":true," +
                 "\"answer_url\":null},{\"id\":2,\"description\":\"desc2\",\"is_correct\":false,\"answer_url\":null}," +
                 "{\"id\":3,\"description\":\"desc3\",\"is_correct\":false,\"answer_url\":\"someurl\"}]}";
-        when(answerService.getAllAnswers()).thenReturn(Arrays.asList(
+        when(answerService.getAnswers(0, 4)).thenReturn(Arrays.asList(
                 new AnswerDTO(1L, "desc", true, null),
                 new AnswerDTO(2L, "desc2", false, null),
                 new AnswerDTO(3L, "desc3", false, "someurl")));
 
-        final MvcResult result = mockMvc.perform(get(AnswerController.ANSWERS_BASE_URL)
+        final MvcResult result = mockMvc.perform(get(AnswerController.ANSWERS_BASE_URL + "?page=0&size=4")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.answers", hasSize(3))).andReturn();
